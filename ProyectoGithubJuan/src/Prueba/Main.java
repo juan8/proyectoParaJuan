dbee2bfe59cb9b44e8a1e72416022ffd7d86d742
juan8 <juan_antonio_13_1992@hotmail.com>
2014-01-23 12:53:27 +0000

    version 45

---
+++ ProyectoGithubJuan/src/Prueba/Main.java
@@ -0,0 +1,14 @@
+package Prueba;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Juan");
+		System.out.println("version 4");
+		
+		
+	}
+
+}
